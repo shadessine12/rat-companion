7b1236277bb1ead71a2547b41e319d2cd252d8e1
Julia <shadessine12>
2024-05-12 10:03:46 -0700

    Transparent, fitted background, start of randomizer

--- main.py
+++ main.py
@@ -2,23 +2,33 @@ import tkinter
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+import random
 '''
 Auuuuuuuu, I guess I am doing this now, for whoever is following along,
 I will probably leave these comments at the end of certain chunks to state my current issues,
 but don't answer, just go bruh how can you have such donut issue
 '''
-
+x = 1400
+width = 73
+height = 108
+idle = [1, 2, 3, 4, 5, 6]
+event_number = random.randrange(1, 6)
 window = tk.Tk()
 window.title("Rat Maid")
 window.iconbitmap(r'C:\Users\julia\Downloads\favicon.ico')
-window.geometry('500x500')
-window.attributes('-alpha',0.75)
+window.geometry('73x108+'+str(x)+'+711')
+window.attributes('-alpha',1.0)
+
+window.config(highlightbackground='black')
+window.overrideredirect(True)
+window.wm_attributes('-transparentcolor', 'black')
+
 '''
 Let's go, I have transparency now
 '''
-char1 = PhotoImage(file = r'C:\Users\julia\Downloads\idle-1-preview.png')
-canv = tkinter.Canvas(window, bg='white', height=500, width=500)
-creation = canv.create_image(250, 250, image=char1, )
+char1 = PhotoImage(file = r'C:\Users\julia\OneDrive\Pictures\idle-1-preview.png')
+canv = tkinter.Canvas(window, bg='black', height=height, width=width)
+creation = canv.create_image(width/2, height/2, image=char1, )
 '''
 Sprite added (it started to fleeping not add because of file= >:(
 , but I figured that out :D)
