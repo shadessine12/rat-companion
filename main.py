fbc9478a9bcccb2270524e22dab13a58a7af938b
Julia <shadessine12>
2024-05-11 17:13:32 -0700

    Creating window

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Auuuuuuuu")
+import tkinter as tk
+from tkinter import ttk
+'''
+Auuuuuuuu, I guess I am doing this now, for whoever is following along,
+I will probably leave these comments at the end of certain chunks to state my current issues,
+but don't answer, just go bruh how can you have such donut issue
+'''
+
+window = tk.Tk()
+window.mainloop()
