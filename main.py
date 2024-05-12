4b6128128886f293125003afacd96b5c4304c3af
Julia <shadessine12>
2024-05-11 17:37:04 -0700

    TRANSPARENCY LESSS GO

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
 '''
 Auuuuuuuu, I guess I am doing this now, for whoever is following along,
 I will probably leave these comments at the end of certain chunks to state my current issues,
@@ -7,4 +8,11 @@ but don't answer, just go bruh how can you have such donut issue
 '''
 
 window = tk.Tk()
+window.title("Rat Maid")
+window.geometry('500x500')
+window.attributes('-alpha',0.5)
+'''
+Let's go, I have transparency now
+'''
+
 window.mainloop()
