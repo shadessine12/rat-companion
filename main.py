802b1f5d7b0bfe70104bc067e03a198e97136f33
Julia <shadessine12>
2024-05-24 21:12:27 -0700

    IT IS ALIVE

--- main.py
+++ main.py
@@ -29,19 +29,24 @@ Let's go, I have transparency now
 canv = tkinter.Canvas(window, bg='black', height=height, width=width)
 char1 = PhotoImage(file = r'C:\Users\julia\OneDrive\Pictures\idle-1-preview.png')
 char2 = PhotoImage(file = r'C:\Users\julia\OneDrive\Pictures\idle-2-preview.png')
-char3 = PhotoImage(file = r'C:\Users\julia\OneDrive\Pictures\idle-3-preview.png')
+char3 = PhotoImage(file = r'C:\Users\julia\OneDrive\Pictures\idle-3-preview-removebg-preview.png')
 char4 = PhotoImage(file = r'C:\Users\julia\OneDrive\Pictures\idle-4-preview.png')
 char5 = PhotoImage(file = r'C:\Users\julia\OneDrive\Pictures\idle-5-preview.png')
 char6 = PhotoImage(file = r'C:\Users\julia\OneDrive\Pictures\idle-6-preview.png')
 idle = [char1, char2, char3, char4, char5, char6]
+counter = 0;
 def idleAni (idle):
     for i in idle:
-        time.sleep(10)
-        canv.create_image(width / 2, height / 2, image=i, )
-        canv.pack()
-        time.sleep(10)
-        canv.delete('all')
-        canv.pack()
+            canv.create_image(width / 2, height / 2, image=i, )
+            window.update_idletasks()
+            window.update()
+            canv.pack()
+            time.sleep(1)
+            canv.delete('all')
+            window.update_idletasks()
+            window.update()
+            canv.pack()
+    idleAni(idle)
 '''
 Sprite added (it started to fleeping not add because of file= >:(
 , but I figured that out :D)
